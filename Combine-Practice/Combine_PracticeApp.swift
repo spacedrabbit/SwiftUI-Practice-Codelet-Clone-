ba7e22a6ec6b083584e9233bc8edab8e895662fb
Louis Tur <louis.tur@gmail.com>
2024-01-14 15:52:44 -0600

    Initial Commit

---
+++ Combine-Practice/Combine_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_PracticeApp.swift
+//  Combine-Practice
+//
+//  Created by Louis Tur on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Combine_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
